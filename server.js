c1ddbbcf3dd69d8bdfd927ca7975e9b1acd04044
olarr-code <horlar567@gmail.com>
2026-09-23 20:38:10 +0100

    Added domain

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express()
 app.use(cors({ 
     origin: [
     "http://localhost:5173",
-    "https://vendly-frontend-theta.vercel.app"
+    "https://vendly-frontend-jade.vercel.app/"
     ]
 }))
 
